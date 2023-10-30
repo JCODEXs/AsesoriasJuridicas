c672aaf9e9f046af8eea2fd3535942f6f13eb606
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-10-30 12:32:38 -0500

    landing

--- components/homePage/servicesSplited.js
+++ components/homePage/servicesSplited.js
@@ -40,7 +40,7 @@ const Services = () => {
       >
         <h3>Pensión de invalidez</h3>
         <Image
-          src="/images/site/balance.jpg"
+          src="/images/site/disability.jpg"
           alt="Imagen 2"
           width={250}
           height={170}
@@ -108,7 +108,7 @@ const Services = () => {
           style={{
             display: "block",
             alignItems: "center",
-            marginBottom: "0.3rem",
+            margin: "0.83rem",
             fontSize: "1.5rem",
             justifyContent: "center",
             alignContent: "center",
@@ -116,20 +116,43 @@ const Services = () => {
         >
           NUESTRO COMPROMISO ES USTED!
         </div>
-        <p>
+        <div style={{
+            display: "flex",
+            flexDirection:"row-reverse",
+            alignItems: "center",
+            marginBottom: "0.3rem",
+            fontSize: "1.2rem",
+            justifyContent: "space-between",
+            alignContent: "space-evenly",
+          }}>
           {" "}
+          <Image
+          src="/images/site/balance.jpg"
+          alt="Imagen 2"
+          width={250}
+          height={250}
+        />
+ <div style={{
+            display: "block",
+            alignItems: "center",
+            margin: "0.5rem",
+            fontSize: "1.1rem",
+            justifyContent: "center",
+            alignContent: "center",
+          }}>
           Tenemos la experiencia para garantizar el éxito en demandas por
           pensión de vejez, invalidez, sobreviviente, tutelas y recursos de
           casación ante la Corte Suprema de Justicia, Sala Laboral.
-        </p>
-        <p>
-          {" "}
+          <p> {" "}
           Para ello, contamos con un equipo de alta calidad humana, comprometido
           con cada gestión que se realice para obtener la pensión a la que tiene
           derecho o la indemnización de perjuicios para los pensionados o
           afiliados que fueron trasladados por engaño de los fondos privados de
-          pensión.
-        </p>
+          pensión. </p>
+        </div>
+       
+         
+        </div>
       </div>
     </div>
   );
