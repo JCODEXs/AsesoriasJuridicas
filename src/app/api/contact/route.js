2fc346df7e69dd5c8f0d54bcc5a55febcd78b9c7
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-11-01 15:40:55 -0500

    wip

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -4,18 +4,46 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import  {EmailTemplate} from "../../../../components/contact/emailTemplate"
 let cached;
-
-
+import nodemailer from 'nodemailer';
+function generateHTML(contact) {
+  return (
+    `<li class="card">
+      ${contact.name ? `<div><strong>Nombre:</strong> ${contact.name}<br /></div>` : ''}
+      ${contact.email ? `<div><strong>Email:</strong> ${contact.email}<br /></div>` : ''}
+      ${contact.message ? `<div><strong>Mensaje:</strong> ${contact.message}<br /></div>` : ''}
+      ${contact.contactDetails ? `<div><strong>Email:</strong> ${contact.contactDetails}<br /></div>` : ''}
+      ${contact.phone ? `<div><strong>Telefono:</strong> ${contact.phone}<br /></div>` : ''}
+      <br />
+    </li>`
+  );
+}
+const {user,pass}=process.env
 
 export async function POST(req, res) {
   const resend = new Resend(process.env.MY_SECRET_KEY);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.privateemail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: user, 
+      pass: pass
+    }
+  });
+  
+  
   try {
     const data = await req.json();
     const jsonData = JSON.stringify(data);
-    
+    const html= generateHTML(data)
     const cached = await connectToDatabase();
     const db = cached.conn.db;
-  
+    const mailOptions = {
+      from: ' PensionesColombianos <juasees@mettalecode.site>',
+      to: ["juansebastianescobar.vega@gmail.com","asesoriastabares@hotmail.com"],
+      subject: "Nuevo Cliente",
+      html: html
+    };
     const result = await db.collection('Contactos').insertOne(data);
     const email = await resend.emails.send({
       from: 'PensionesColombianos <onboarding@resend.dev>',
@@ -23,6 +51,16 @@ export async function POST(req, res) {
       subject: 'Nuevo Cliente',
       react: EmailTemplate({contact:data}),
     });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error)
+        return NextResponse.json({ message: "error occurred" }, { status: 400 });
+      } else {
+        console.log("hi")
+        return NextResponse.json({ data: jsonData }, { status: 200 });
+      }
+    });
+  
     console.log(email)
     return NextResponse.json({ data: jsonData }, { status: 200 });
   } catch (err) {
---
+++ src/app/api/sendEmail/route.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+export default async (req, res) => {
+  const { body } = req;
+
+  const transporter = nodemailer.createTransport({
+    host: 'mail.mettalecode.sites',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'juasees@mettalecode.site', 
+      pass: 'elegido22'
+    }
+  });
+
+  const mailOptions = {
+    from: 'PensionesColombianos',
+    to: "juansebastianescobar.vega@gmail.com",
+    subject: "New client",
+    text: body.message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send({ success: false });
+    } else {
+      res.status(200).send({ success: true });
+    }
+  });
+};
