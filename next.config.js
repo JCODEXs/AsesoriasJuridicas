b9d945d7fc43d48627870d5c23e4c613e8edf721
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-07-26 16:31:41 -0500

    canvas

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
-//     if (isServer) {
-//       config.externals = [
-//         {
-//           canvas: 'canvas', // Exclude canvas
-//           'next/font/google/target.css': 'next/font/google/target.css', // Exclude next/font
-//         },
-//         nodeExternals({
-//           allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i], // Only bundle files that are not .js, .jsx, or .json
-//         }),
-//       ];
-//     }
+    //     if (isServer) {
+    //       config.externals = [
+    //         {
+    //           canvas: 'canvas', // Exclude canvas
+    //           'next/font/google/target.css': 'next/font/google/target.css', // Exclude next/font
+    //         },
+    //         nodeExternals({
+    //           allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i], // Only bundle files that are not .js, .jsx, or .json
+    //         }),
+    //       ];
+    //     }
 
     // Add a rule for .node files
     config.module.rules.push({
       test: /\.node$/,
-      use: 'node-loader', // or 'file-loader' if you prefer
+      use: "node-loader", // or 'file-loader' if you prefer
     });
 
     // Add a rule for .jsx files
-//     config.module.rules.push({
-//       test: /\.jsx?$/,
-//       exclude: /node_modules/,
-//       use: {
-//         loader: 'babel-loader',
-//         options: {
-//           presets: ['@babel/preset-react'],
-//         },
-//       },
-//     });
+    //     config.module.rules.push({
+    //       test: /\.jsx?$/,
+    //       exclude: /node_modules/,
+    //       use: {
+    //         loader: 'babel-loader',
+    //         options: {
+    //           presets: ['@babel/preset-react'],
+    //         },
+    //       },
+    //     });
 
     return config;
   },
