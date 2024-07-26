8c0c47d549423594f09815926f44b46c95dbc383
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-07-26 16:44:13 -0500

    wip

--- next.config.js
+++ next.config.js
@@ -1,25 +1,50 @@
-const nodeExternals = require("webpack-node-externals");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    //     if (isServer) {
-    //       config.externals = [
-    //         {
-    //           canvas: 'canvas', // Exclude canvas
-    //           'next/font/google/target.css': 'next/font/google/target.css', // Exclude next/font
-    //         },
-    //         nodeExternals({
-    //           allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i], // Only bundle files that are not .js, .jsx, or .json
-    //         }),
-    //       ];
-    //     }
+    if (isServer) {
+      config.externals = [
+        {
+          canvas: 'canvas', // Exclude canvas
+        },
+        nodeExternals({
+          allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i], // Only bundle files that are not .js, .jsx, or .json
+        }),
+      ];
+    }
 
     // Add a rule for .node files
     config.module.rules.push({
       test: /\.node$/,
-      use: "node-loader", // or 'file-loader' if you prefer
+      use: 'node-loader',
     });
+
+    return config;
+  },
+};
+// const nodeExternals = require("webpack-node-externals");
+
+// module.exports = {
+//   reactStrictMode: true,
+//   webpack: (config, { isServer }) => {
+//     //     if (isServer) {
+//     //       config.externals = [
+//     //         {
+//     //           canvas: 'canvas', // Exclude canvas
+//     //           'next/font/google/target.css': 'next/font/google/target.css', // Exclude next/font
+//     //         },
+//     //         nodeExternals({
+//     //           allowlist: [/\.(?!(?:jsx?|json)$).{1,5}$/i], // Only bundle files that are not .js, .jsx, or .json
+//     //         }),
+//     //       ];
+//     //     }
+
+//     // Add a rule for .node files
+//     config.module.rules.push({
+//       test: /\.node$/,
+//       use: "node-loader", // or 'file-loader' if you prefer
+//     });
 
     // Add a rule for .jsx files
     //     config.module.rules.push({
