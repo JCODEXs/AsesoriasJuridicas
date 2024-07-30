46885f473a9e77c752f7cf149388fdc16c7c29d9
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-07-30 10:37:35 -0500

    wip

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,44 +1,57 @@
-"use client"
+"use client";
 import { useAuth, useUser } from "@clerk/nextjs";
 import UserList from "../../../components/contact/contactsList";
 import { useEffect, useState } from "react";
 // import fetchData from "../../../lib/posts-util";
 
-
-
 export default function Admin(users) {
   const { isLoaded, userId, sessionId, getToken } = useAuth();
-  const {user}=useUser()
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(true)
+  const { user } = useUser();
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
   const userEmail = user?.emailAddresses[0]?.emailAddress;
-  const targetEmailAddresses=["juansebastianescobar.vega@gmail.com","dianamarcelamolinamera@gmail.com","asesoriastabares@hotmail.com "]
+  const targetEmailAddresses = [
+    "juansebastianescobar.vega@gmail.com",
+    "dianamarcelamolinamera@gmail.com",
+    "asesoriastabares@hotmail.com ",
+  ];
   let isMatch = false;
   if (userEmail && targetEmailAddresses.includes(userEmail)) {
-    isMatch = true;}
- console.log(data)
+    isMatch = true;
+  }
+  //  console.log(data)
   useEffect(() => {
-    fetch('/api/contact',{ next:{revalidate:300}})
+    fetch("/api/contact", { next: { revalidate: 300 } })
       .then((res) => res.json())
       .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
 
- 
-  if (isLoading) return <p>Loading...</p>
-  if (!data ) return (<div style={{minHeight:"66vh"}}><p>No profile data</p></div>)
- 
+  if (isLoading) return <p>Loading...</p>;
+  if (!data)
+    return (
+      <div style={{ minHeight: "66vh" }}>
+        <p>No profile data</p>
+      </div>
+    );
+
   // In case the user signs out while on the page.
-  if (!isLoaded &&user  || user && !isMatch) {
-    return (<div style={{minHeight:"66vh"}}><p>Bienvenido{user.username?user.username:user.id} !Gracias por hacer parte de nuestra comunidad!!  </p></div>)
+  if ((!isLoaded && user) || (user && !isMatch)) {
+    return (
+      <div style={{ minHeight: "66vh" }}>
+        <p>
+          Bienvenido{user.username ? user.username : user.id} !Gracias por hacer
+          parte de nuestra comunidad!!{" "}
+        </p>
+      </div>
+    );
   }
 
-  
   return (
-    <div style={{minHeight:"66vh", marginTop:"1rem"}}>
-     <UserList contacts={data}/>
+    <div style={{ minHeight: "66vh", marginTop: "1rem" }}>
+      <UserList contacts={data} />
     </div>
   );
 }
--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,67 +1,69 @@
 import { ObjectId } from "mongodb";
-import { connectToDatabase} from "../../../../lib/mongoDb";
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
-import  {EmailTemplate} from "../../../../components/contact/emailTemplate"
+import { connectToDatabase } from "../../../../lib/mongoDb";
+import { NextResponse } from "next/server";
+// import { Resend } from 'resend';
+// import  {EmailTemplate} from "../../../../components/contact/emailTemplate"
 let cached;
-import nodemailer from 'nodemailer';
-function generateHTML(contact) {
-  return (
-    `<li class="card">
-      ${contact.name ? `<div><strong>Nombre:</strong> ${contact.name}<br /></div>` : ''}
-      ${contact.email ? `<div><strong>Email:</strong> ${contact.email}<br /></div>` : ''}
-      ${contact.message ? `<div><strong>Mensaje:</strong> ${contact.message}<br /></div>` : ''}
-      ${contact.contactDetails ? `<div><strong>Email:</strong> ${contact.contactDetails}<br /></div>` : ''}
-      ${contact.phone ? `<div><strong>Telefono:</strong> ${contact.phone}<br /></div>` : ''}
-      <br />
-    </li>`
-  );
-}
-const {user,pass}=process.env
+// import nodemailer from 'nodemailer';
+// function generateHTML(contact) {
+//   return (
+//     `<li class="card">
+//       ${contact.name ? `<div><strong>Nombre:</strong> ${contact.name}<br /></div>` : ''}
+//       ${contact.email ? `<div><strong>Email:</strong> ${contact.email}<br /></div>` : ''}
+//       ${contact.message ? `<div><strong>Mensaje:</strong> ${contact.message}<br /></div>` : ''}
+//       ${contact.contactDetails ? `<div><strong>Email:</strong> ${contact.contactDetails}<br /></div>` : ''}
+//       ${contact.phone ? `<div><strong>Telefono:</strong> ${contact.phone}<br /></div>` : ''}
+//       <br />
+//     </li>`
+//   );
+// }
+const { user, pass } = process.env;
 
 export async function POST(req, res) {
-  const resend = new Resend(process.env.MY_SECRET_KEY);
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.privateemail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: user, 
-      pass: pass
-    }
-  });
-  
-  
+  // const resend = new Resend(process.env.MY_SECRET_KEY);
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.privateemail.com',
+  //   port: 587,
+  //   secure: false, // true for 465, false for other ports
+  //   auth: {
+  //     user: user,
+  //     pass: pass
+  //   }
+  // });
+
   try {
     const data = await req.json();
     const jsonData = JSON.stringify(data);
-    const html= generateHTML(data)
+    const html = generateHTML(data);
     const cached = await connectToDatabase();
     const db = cached.conn.db;
     const mailOptions = {
-      from: ' PensionesColombianos <juasees@mettalecode.site>',
-      to: ["juansebastianescobar.vega@gmail.com","asesoriastabares@hotmail.com"],
+      from: " PensionesColombianos <juasees@mettalecode.site>",
+      to: [
+        "juansebastianescobar.vega@gmail.com",
+        "asesoriastabares@hotmail.com",
+      ],
       subject: "Nuevo Cliente",
-      html: html
+      html: html,
     };
-    const result = await db.collection('Contactos').insertOne(data);
-    const email = await resend.emails.send({
-      from: 'PensionesColombianos <onboarding@resend.dev>',
-      to: "juansebastianescobar.vega@gmail.com",
-      subject: 'Nuevo Cliente',
-      react: EmailTemplate({contact:data}),
-    });
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error)
-        return NextResponse.json({ message: "error occurred" }, { status: 400 });
-      } else {
-        console.log("hi")
-        return NextResponse.json({ data: jsonData }, { status: 200 });
-      }
-    });
-  
-    console.log(email)
+    const result = await db.collection("Contactos").insertOne(data);
+    // const email = await resend.emails.send({
+    //   from: 'PensionesColombianos <onboarding@resend.dev>',
+    //   to: "juansebastianescobar.vega@gmail.com",
+    //   subject: 'Nuevo Cliente',
+    //   react: EmailTemplate({contact:data}),
+    // });
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     console.log(error)
+    //     return NextResponse.json({ message: "error occurred" }, { status: 400 });
+    //   } else {
+    //     console.log("hi")
+    //     return NextResponse.json({ data: jsonData }, { status: 200 });
+    //   }
+    // });
+
+    console.log(email);
     return NextResponse.json({ data: jsonData }, { status: 200 });
   } catch (err) {
     console.log(err);
@@ -73,10 +75,10 @@ export async function GET(req, res) {
   try {
     const cached = await connectToDatabase();
     const db = cached.conn.db;
-    const result = await db.collection('Contactos').find().toArray();
-    const result2 = await db.collection('Email').find().toArray();
+    const result = await db.collection("Contactos").find().toArray();
+    const result2 = await db.collection("Email").find().toArray();
     const combinedResults = [...result, ...result2];
-    console.log(result)
+    console.log(result);
     return NextResponse.json({ combinedResults }, { status: 200 });
   } catch (err) {
     console.log(err);
@@ -88,23 +90,39 @@ export async function DELETE(req, res) {
   try {
     const data = await req.json();
     const jsonData = JSON.stringify(data);
-    
+
     const cached = await connectToDatabase();
     const db = cached.conn.db;
 
     if (data.name) {
-      const result = await db.collection('Contactos').deleteOne({ _id: new ObjectId(data._id) });
+      const result = await db
+        .collection("Contactos")
+        .deleteOne({ _id: new ObjectId(data._id) });
       if (result.deletedCount === 1) {
-        return NextResponse.json({ message: "Successfully deleted the record from Contactos" }, { status: 200 });
+        return NextResponse.json(
+          { message: "Successfully deleted the record from Contactos" },
+          { status: 200 }
+        );
       } else {
-        return NextResponse.json({ message: "No record found for deletion in Contactos" }, { status: 404 });
+        return NextResponse.json(
+          { message: "No record found for deletion in Contactos" },
+          { status: 404 }
+        );
       }
     } else {
-      const result = await db.collection('Email').deleteOne({ _id: new ObjectId(data._id) });
+      const result = await db
+        .collection("Email")
+        .deleteOne({ _id: new ObjectId(data._id) });
       if (result.deletedCount === 1) {
-        return NextResponse.json({ message: "Successfully deleted the record from Email" }, { status: 200 });
+        return NextResponse.json(
+          { message: "Successfully deleted the record from Email" },
+          { status: 200 }
+        );
       } else {
-        return NextResponse.json({ message: "No record found for deletion in Email" }, { status: 404 });
+        return NextResponse.json(
+          { message: "No record found for deletion in Email" },
+          { status: 404 }
+        );
       }
     }
   } catch (err) {
